ce253404d0f489805e3eb8e1439db0528fcecc3a
RaimundoJSoares <raianimes1000@gmail.com>
2021-12-20 02:41:48 -0400

    for loop

--- js/loop_for.js
+++ js/loop_for.js
@@ -11,4 +11,28 @@ do {
 while(i < 5)
 
 //for , while, do while
+//key value
+const pessoa = {
+  nome:'chloe', //key ou chave é o nome, e value é a resposta
+  profissao: 'vtuber'
+}
 
+for(let key in pessoa) {
+  console.log(key, pessoa['nome']);
+}
+
+const cores = ['azul', 'vermelho', 'preto', 'verde']
+
+for (let indice in cores) {
+  console.log(indice, cores[indice]);
+}
+
+//for of  - melhor opção
+for (let cor of cores){
+  console.log(cor);
+}
+
+const objetos = ['garrafa', 'pc', 'teclado', 'mouse', 'cadeira']
+for (let coisas of objetos){
+  console.log(coisas)
+}
