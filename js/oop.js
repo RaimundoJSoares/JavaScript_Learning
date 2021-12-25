25a974696c7067d293d72c3c6a475cf67c06b8a3
RaimundoJSoares <raianimes1000@gmail.com>
2021-12-25 12:17:36 -0400

    OOP

--- js/oop.js
+++ js/oop.js
@@ -5,30 +5,35 @@ class Person {
   }
 }
 //user sempre o "NEW" quando chamar a classe
-fullname = new Person("sakamata", "chloe")
-console.log(fullname.firstName) 
-console.log(fullname.lasName)
+let fullname = new Person("sakamata", "chloe")
+console.log(fullname.firstName, fullname.lasName) 
 
+//Methods
+class Persontwo{
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+  greet() {
+    console.log('Olá',this.firstname, this.lastname,'-san');
+  }
+}
+let greetings = new Persontwo('Sakamata', 'Chloe');
+console.log(greetings.greet()); // mnão esquecer de usar o parenteses ja que é function
 
- class Persona {
-   constructor(firstName, LastName, date) {
-     this.firstName = firstName;
-     this.LastName = LastName;
-     this.setDateOfBirth(date);
-   }
+class Persona {
+  constructor(firstname, lastname){
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+  greet() {
+    console.log('Hello ', this.firstname, this.lastname, '-san');
+  }
+  wishes(wish) {
+    return "I wish to you a wonderful " + wish;
+  }
+}
+let message = new Persona('Sakamata', 'Chloe');
+let happy = message.wishes('Merry Christmas')
+console.log(happy, message.greet())
 
-   getDateofBirth(){
-     return this._dateOfBirth;
-   }
-   setDateofBirth(value){
-     if (value instanceof Date && value < Date.now()){
-       this._dateOfBirth = value;
-     }
-     else {
-       throw new TypeError('Birthdate cannot be in the futue');
-
-     }
-   }
- }
- const newperson = new Persona('sakamata', 'Chloe', new Date(2021,11,11));
- console.log(newperson.getDateofBirth());
